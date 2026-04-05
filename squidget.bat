5e0de850a678698e479401970b1b18c2a7293ee1
Lollollolmymy <yolowuloq@gmail.com>
2026-04-05 18:48:46 -0400

    Add back squidget.bat as primary build script

---
+++ squidget.bat
@@ -0,0 +1,60 @@
+@echo off
+REM Universal SquidGet Builder - works from any old downloaded version directory
+
+REM Find the deepest squidget repo (handles nested structures)
+setlocal enabledelayedexpansion
+cd /d "%~dp0"
+
+:find_repo
+if exist "main.c" goto found_repo
+if exist "SquidGet\main.c" (
+    cd SquidGet
+    goto found_repo
+)
+if exist "SquidGet-main\main.c" (
+    cd SquidGet-main
+    goto found_repo
+)
+
+echo Error: Could not find SquidGet source files  
+pause
+exit /b 1
+
+:found_repo
+set "DIR=%cd%\"
+set "TCC=%DIR%tcc\tcc.exe"
+set "IMPDEF=%DIR%tcc\tiny_impdef.exe"
+set "EXE=%DIR%squidget.exe"
+
+if exist "%EXE%" (
+    echo [squidget] launching...
+    goto run
+)
+
+echo [squidget] compiling...
+
+"%IMPDEF%" C:\Windows\System32\winhttp.dll -o "%DIR%tcc\lib\winhttp.def"
+
+set SQT_DEBUG=1
+if "%SQT_DEBUG%"=="1" (set DBG_FLAG=-DSQT_DEBUG) else (set DBG_FLAG=)
+
+"%TCC%" -O2 %DBG_FLAG% ^
+    "%DIR%main.c" "%DIR%api.c" "%DIR%download.c" ^
+    "%DIR%tui.c"  "%DIR%json.c" "%DIR%config.c" "%DIR%platform.c" ^
+    "%DIR%tcc\lib\winhttp.def" ^
+    -I"%DIR%" -I"%DIR%tcc\include" -I"%DIR%tcc\include\winapi" ^
+    -L"%DIR%tcc\lib" ^
+    -lshell32 -lole32 -lm ^
+    -o "%EXE%"
+
+if errorlevel 1 (
+    if exist "%EXE%" del "%EXE%"
+    echo [squidget] compilation failed.
+    pause
+    exit /b 1
+)
+
+echo [squidget] done.
+
+:run
+"%EXE%"
